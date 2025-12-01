adaf2f24b8705caa8f2df2adb51e68cfa5e2d33d
Kevin Tang <kevintang1118@outlook.com>
2025-12-01 08:38:14 -0800

    Update projects/kinde-angular/src/lib/kinde-angular.service.ts
    Co-authored-by: Luuk Haijes <luuk_haijes@yahoo.com>

--- projects/kinde-angular/src/lib/kinde-angular.service.ts
+++ projects/kinde-angular/src/lib/kinde-angular.service.ts
@@ -133,7 +133,7 @@ export class KindeAngularService implements OnDestroy {
     this.document.location.href = registerUrl.href;
   }
 
-  async register_with_org(options?: RegisterURLOptions): Promise<void> {
+  async registerWithOrg(options?: RegisterURLOptions): Promise<void> {
     if(!this.kindeClient) throw new Error("kindeClient is null");
     const registerUrl = await this.kindeClient.createOrg(options);
     this.document.location.href = registerUrl.href;
